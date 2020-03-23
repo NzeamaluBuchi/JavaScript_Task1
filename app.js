cfa2662a69b70063570bd9f698da5d8a58d97ba5
Nzeamalu Onyebuchi Emmanuel <redreligion32@gmail.com>
2020-03-23 01:34:42 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let name = "Nzeamalu Onyebuchi Emmanuel";
+
+let coursesOffered = [" HTML ", " CSS ", " JAVASCRIPT "]
+
+document.write("My name is " + name + " and these are the courses i am taking: " + coursesOffered + ".");
+
+ let numbers = [];
+    for (let i=1; i<200; i++) {
+        if(i % 2 !== 0) {
+          numbers.push(i);
+        }
+    }
+document.write(" These are the odd numbers form 1-200: " + numbers);
